f80c8a96fc4576845d929f9671d7ba91bf4a302f
Andrzej <ekspensum@interia.pl>
2018-04-07 21:31:08 +0200

    Zadania5 - licznik słów

--- zadania5/src/LiczZnaki.java
+++ zadania5/src/LiczZnaki.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -8,12 +9,16 @@ public class LiczZnaki {
 
     public static void main(String[] args) {
         File f = new File("jakisPlik.txt");
-        int tab[] = new int[4];
+        int tab[] = new int[5];
         String temp;
         Scanner fs, fs2, fs3, fs4, fs5;
-        char [] znaki;
-        Pattern wzorzec = Pattern.compile("^([a-zA-Z0-9]+\\s+){1} || (\\s+[a-zA-Z0-9]+\\s+)+ || (\\s+[a-zA-Z0-9]+)$");
+        char[] znaki;
+        StringTokenizer st;
+
+//        Pattern wzorzec = Pattern.compile("^([a-zA-Z0-9]+\\s){1}.*([a-zA-Z0-9]+){1}$");
+        Pattern wzorzec = Pattern.compile("(\\s[a-zA-Z0-9]+\\s)+");
         Matcher dopasuj;
+
         try {
             fs = new Scanner(f);
             fs2 = new Scanner(f);
@@ -24,17 +29,20 @@ public class LiczZnaki {
                 tab[0]++;
                 tab[1] += fs.nextLine().length();
                 znaki = fs2.nextLine().toCharArray();
-                for (int i=0; i<znaki.length; i++){
+                for (int i = 0; i < znaki.length; i++) {
                     if (znaki[i] == ' ' || znaki[i] == '\t' || znaki[i] == '\n') tab[2]++;
-
                 }
-                dopasuj = wzorzec.matcher(fs5.nextLine());
-                if (dopasuj.find()) tab[3] += dopasuj.groupCount();
-//                System.out.println(dopasuj.groupCount());
-//                if (fs3.nextLine().matches("^[a-zA-Z0-9]+\\s+(\\s+[a-zA-Z0-9]+\\s+)*\\s?[a-zA-Z0-9]+$")) tab[3]++;
-//                System.out.println(fs4.nextLine().matches("^([a-zA-Z0-9]+\\s+){1} | (\\s+[a-zA-Z0-9]+\\s+)+ | (\\s+[a-zA-Z0-9]+)$"));
-                System.out.println(fs3.nextLine());
+                st = new StringTokenizer(fs3.nextLine());
+                tab[3] += st.countTokens();
 
+                dopasuj = wzorzec.matcher(fs4.nextLine());
+
+                if (dopasuj.find()) {
+                    System.out.println(dopasuj.group());
+                    tab[4]++;
+                }
+
+                System.out.println(fs5.nextLine());
             }
 
             fs.close();
@@ -47,8 +55,9 @@ public class LiczZnaki {
         System.out.println();
         System.out.println("Liczba linii: " + tab[0]);
         System.out.println("Liczba znaków: " + tab[1]);
-        System.out.println("Liczba białych znaków: " + (tab[2]+tab[0]));
+        System.out.println("Liczba białych znaków: " + (tab[2] + tab[0]));
         System.out.println("Liczba słów: " + tab[3]);
+        System.out.println("Cwiczenia regex: "+tab[4]);
 
     }
 }
